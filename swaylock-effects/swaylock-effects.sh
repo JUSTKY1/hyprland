216f2577a294e71682aed5e5878b24fc1ab94bd5
Justin <justinthornton39@gmail.com>
2023-08-29 14:19:44 -0700

    Add files via upload

---
+++ swaylock-effects/swaylock-effects.sh
@@ -0,0 +1,16 @@
+swaylock \
+	--screenshots \
+	--clock \
+	--timestr %c \
+	--indicator \
+	--indicator-radius 250 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color bb00cc \
+	--key-hl-color 880033 \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--grace 0 \
+	--fade-in 0.001
